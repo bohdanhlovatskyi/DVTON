546e515dce6634f9e79d183eff81044feb1ce140
bhdanhlovatskyi <bohdan.hlovatskyi@cu.edu.ua>
2023-04-12 20:30:24 +0300

    added some diffusers

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -14,6 +14,33 @@ from torchmetrics.image.ssim import StructuralSimilarityIndexMeasure
 from torchmetrics.image.fid import FrechetInceptionDistance
 from torchmetrics.image.kid import KernelInceptionDistance
 
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+
+class EvalDataset(Dataset):
+    def __init__(self, predict_dir='../external/HR-VITON/Output', gt_dir='../data/hrviton/test/image', resolution=1024):
+        self.predict_dir = predict_dir
+        self.gt_dir = gt_dir
+        self.resolution = resolution
+
+        self.gt_images = list(np.sort(os.listdir(self.gt_dir)))
+        self.gt_images = list(map(lambda path: os.path.join(self.gt_dir, path), self.gt_images))
+        self.predict_images = list(np.sort(os.listdir(self.predict_dir)))
+        self.predict_images = list(map(lambda path: os.path.join(self.predict_dir, path), self.predict_images))
+    
+    def __len__(self):
+        return len(self.gt_images)
+    
+    def read_image(self, path):
+        image = np.array(Image.open(path))
+        image = Transforms.ToTensor()(image)
+        return image
+
+    def __getitem__(self, idx):
+        return (self.read_image(self.predict_images[idx]), self.read_image(self.gt_images[idx]))
+
+
 def get_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--metric', default='LPIPS')
@@ -24,35 +51,24 @@ def get_opt():
 
     return opt
 
-ToTensor = Transforms.ToTensor()
-
 @torch.no_grad()
 def eval(opt):
-    pred_list = sorted(os.listdir(opt.predict_dir))
-    gt_list = sorted(os.listdir(opt.ground_truth_dir))
+    dataset = EvalDataset(predict_dir=opt.predict_dir, gt_dir=opt.ground_truth_dir)
+    dataloader = DataLoader(dataset,
+                            batch_size=16,
+                            shuffle=False,
+                            num_workers=4,
+                            pin_memory=True)
 
     ssim = StructuralSimilarityIndexMeasure()
     lpips = LearnedPerceptualImagePatchSimilarity(net_type='alex')
-    kid = KernelInceptionDistance(subset_size=2)
+    kid = KernelInceptionDistance(subset_size=100)
     fid = FrechetInceptionDistance()
 
-    for i, _ in tqdm(enumerate(pred_list)):
-        print(pred_list[i], gt_list[i])
-        pred_img = np.array(
-            Image.open(os.path.join(opt.predict_dir, pred_list[i]))
-        )
+    print(f"dataset length: {len(dataset)}")
 
-        gt_img = np.array(
-            Image.open(os.path.join(opt.ground_truth_dir, gt_list[i]))
-        )
-
-        import matplotlib.pyplot as plt
-        fig, ax = plt.subplots(1, 2, figsize=(20, 16))
-        ax[0].imshow(pred_img)
-        ax[1].imshow(gt_img)
-        plt.savefig(f"test/test_{i}.png")
-
-        pred_img, gt_img = ToTensor(pred_img)[None], ToTensor(gt_img)[None]
+    for _, sample in tqdm(enumerate(dataloader)):
+        pred_img, gt_img = sample
         
         ssim.update(pred_img, gt_img)
         kid.update((gt_img * 255).to(torch.uint8), real=True)
@@ -63,14 +79,10 @@ def eval(opt):
 
         lpips.update((gt_img * 2) - 1, (pred_img * 2) - 1)
 
-        if i == 10:
-            break
-
     print("average ssim: ", ssim.compute().item())
-    print("average kid: ", kid.compute()[0].item())
+    print("average kid: ", kid.compute()[0].item() * 100)
     print("average fid: ", fid.compute().item())
     print("average lpips: ", lpips.compute().item())
-
 
 if __name__ == "__main__":
     opt = get_opt()
